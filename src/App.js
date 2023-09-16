a1323532544f44fdd0518138b6e4640d8d38b4d5
Soichiro Inatani <inai17ibar@gmail.com>
2023-09-16 22:08:12 +0900

    go-weed-backendのTodoサンプルのフロント機能の基礎を実装した。

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState({
+    title: '',
+    completed: false,
+  }); //
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8081/todos')
+    .then(response => {
+      setTodos(response.data);
+    });
+  }, []);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios.post('http://127.0.0.1:8081/todos', { title: newTodo.title, completed: newTodo.completed })
+    .then(response => {
+      //setTodos([...todos, response.data]);
+      setNewTodo({
+        title: "",
+        completed: false
+      });
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ToDo List</h1>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.title}>
+            {todo.title} : {todo.done ? 'Done' : 'Not done'}
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newTodo.title}
+          onChange={e => setNewTodo({ ...newTodo, title: e.target.value })}
+        />
+        <input
+          type="checkbox"
+          checked={newTodo.completed}
+          onChange={e => setNewTodo({ ...newTodo, completed: e.target.checked })}
+        />
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 }
-
-export default App;

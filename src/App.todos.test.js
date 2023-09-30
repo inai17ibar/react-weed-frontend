ea2a2b73923343315a8f858618ec41a00eb35d20
Soichiro Inatani <inai17ibar@gmail.com>
2023-09-30 12:05:27 +0900

    Reactのtodosのテスト。deleteのレンダリングの待ち時間も２秒に調整した。

---
+++ src/App.todos.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import App from './App';
+import '@testing-library/jest-dom';
+
+// サーバーをセットアップして、エンドポイントをモックします。
+let mockTodos = [
+    { ID: 1, Title: 'Todo 1', Completed: false },
+    { ID: 2, Title: 'Todo 2', Completed: true },
+];
+
+const server = setupServer(
+  rest.get('http://127.0.0.1:8081/todos', (req, res, ctx) => {
+    console.log(mockTodos);
+    return res(ctx.json(mockTodos)); // モックのTodosをレスポンスとして返す
+  }),
+);
+
+// テストが終了したら、モックのTodosをリセット
+afterEach(() => {
+  mockTodos = [];
+});
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test('Get todos', async () => {
+  render(<App />);
+
+  await waitFor(() => {
+    expect(screen.getByText('Todo 1', {}, { timeout: 2000 })).toBeInTheDocument();
+  });
+  await waitFor(() => {
+    expect(screen.getByText('Todo 2', {}, { timeout: 1000 })).toBeInTheDocument();
+  });
+});
